0c93af29ff0e30bba51cb63f22bbb0ed1724a238
ankit <ankit.dumka@mail.valuecoders.com>
2021-01-05 15:59:27 +0530

    remove space

--- frontend/src/component/List_Job/listJob.js
+++ frontend/src/component/List_Job/listJob.js
@@ -61,7 +61,7 @@ class JobList extends Component {
                                     <th>S.No</th>
                                     <th>Job Title</th>
                                     <th>Job Location</th>
-                                    <th>Job Description</th>
+                                    <th>Job Description</th> 
                                     <th>Date</th>
                                     <th>Job status</th>
                                     <th>Action</th>
